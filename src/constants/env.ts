631c0e8cc973a91abc6d53b91a93dd22a4bd3eb4
이봉균 <bglee@rsquare.co.kr>
2017-09-22 15:48:29 +0900

    add _document support ga, na, bootstrap 4

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,11 @@
 export const DEV = process.env.NODE_ENV !== 'production'
 
-export const GA_TRACKING_ID = '';
-export const NA_TRACKING_ID = '';
+export const GA_TRACKING_ID = ''
+export const NA_TRACKING_ID = ''
+export const FB_TRACKING_ID = ''
+export const SENTRY_TRACKING_ID = ''
+
+export const SITE_NAME = ''
+export const SITE_TITLE = ''
+export const SITE_DESCRIPTION = ''
+export const SITE_IMAGE = ''
