7a7b396fe725e77677b8285933b5982dc8f8df8a
이봉균 <bglee@rsquare.co.kr>
2017-10-02 18:19:32 +0900

    font font-awesome

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,12 @@ export default class RSquareDocument extends Document {
         <meta property="twitter:image" content={SITE_IMAGE}/>
 
         <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          {...{crossOrigin: 'anonymous'}}
+        />
+        <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
           integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
