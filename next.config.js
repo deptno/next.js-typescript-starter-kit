ce7ad60c56323bf7ec290006feca808e07ad737f
이봉균 <bglee@rsquare.co.kr>
2017-10-09 02:43:59 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,37 +1,31 @@
+const path            = require('path')
+const trash           = require('trash')
 const {exportPathMap} = require('nextjs-export-path-map')
-const trash = require('trash')
 
 module.exports = {
-    webpack(config) {
-        if (process.env.ANALYZE) {
-            config.plugins.push(new BundleAnalyzerPlugin({
-                analyzerMode: 'server',
-                analyzerPort: 8888,
-                openAnalyzer: true
-            }))
+  webpack(config) {
+    if (process.env.ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server', analyzerPort: 8888, openAnalyzer: true
+      }))
+    }
+    config.module.rules.push({
+      test: /\.p?css$/, use: [
+        {
+          loader: 'emit-file-loader', options: {
+          name: 'dist/[path][name].[ext]'
         }
-        config.module.rules.push({
-            test: /\.p?css$/,
-            use: [
-                {
-                    loader: 'emit-file-loader',
-                    options: {
-                        name: 'dist/[path][name].[ext]'
-                    }
-                },
-                {
-                    loader: 'skeleton-loader',
-                    options: {
-                        procedure   : function (stylesheet) {
-                            trash(this._module.userRequest + '.json')
-                            return 'exports.default = \'' + stylesheet + '\';'
-                        }, cacheable: false
-                    }
-                },
-                'postcss-loader'
-            ]
-        })
-        return config
-    },
-    exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages'))
+        }, {
+          loader: 'skeleton-loader', options: {
+            procedure   : function (stylesheet) {
+              trash(this._module.userRequest + '.json')
+              return 'exports.default = \'' + stylesheet + '\';'
+            }, cacheable: false
+          }
+        }, 'postcss-loader'
+      ]
+    })
+    return config
+  },
+  exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages'))
 }
