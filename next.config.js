0f9fdcd29eb0005a98a67666836aa81242395840
이봉균 <bglee@rsquare.co.kr>
2018-02-06 13:09:31 +0900

    apply next.js/5 apply first class typescript

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 const path            = require('path')
+const withTypescript  = require('@zeit/next-typescript')
 const {exportPathMap} = require('nextjs-export-path-map')
 
-module.exports = {
+module.exports = withTypescript({
   webpack(config) {
     if (process.env.ANALYZE) {
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'server',
-          analyzerPort: 8888,
-          openAnalyzer: true
-        })
-      )
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server', analyzerPort: 8888, openAnalyzer: true
+      }))
     }
     return config
   },
   exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages')),
-}
+})
