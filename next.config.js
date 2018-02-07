1e0e10ca928590b31237063ea3bb8f1398fbe5f8
이봉균 <bglee@rsquare.co.kr>
2018-02-07 13:41:04 +0900

    #3 apply next-css

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 const path            = require('path')
 const withTypescript  = require('@zeit/next-typescript')
+const withCSS         = require('@zeit/next-css')
 const {exportPathMap} = require('nextjs-export-path-map')
 
-module.exports = withTypescript({
-  webpack(config) {
-    if (process.env.ANALYZE) {
-      config.plugins.push(new BundleAnalyzerPlugin({
-        analyzerMode: 'server', analyzerPort: 8888, openAnalyzer: true
-      }))
-    }
-    return config
-  },
-  exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages')),
-})
+module.exports = withTypescript(
+  withCSS({
+    webpack(config) {
+      if (process.env.ANALYZE) {
+        config.plugins.push(new BundleAnalyzerPlugin({
+          analyzerMode: 'server', analyzerPort: 8888, openAnalyzer: true
+        }))
+      }
+      return config
+    },
+    cssModules: true,
+    exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages')),
+  })
+)
--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -11,6 +11,10 @@ declare module '*.svg' {
   const resource: string
   export = resource
 }
+declare module '*.css' {
+  const resource: any
+  export default resource
+}
 declare module '*.pcss' {
   const resource: string
   export default resource
