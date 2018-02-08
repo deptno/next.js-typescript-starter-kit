ca63e7542fe3c56c9bc29a179050eba7681c8d10
이봉균 <bglee@rsquare.co.kr>
2018-02-08 20:26:23 +0900

    cleanup package

--- next.config.js
+++ next.config.js
@@ -3,15 +3,19 @@ const withTypescript  = require('@zeit/next-typescript')
 const withCSS         = require('@zeit/next-css')
 const {exportPathMap} = require('nextjs-export-path-map')
 
-module.exports = withTypescript(withCSS({
-  webpack(config) {
-    if (process.env.ANALYZE) {
-      config.plugins.push(new BundleAnalyzerPlugin({
-        analyzerMode: 'server', analyzerPort: 8888, openAnalyzer: true
-      }))
-    }
-    return config
-  },
-  cssModules: true,
-  exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages')),
-}))
+module.exports = withTypescript(
+  withCSS({
+    webpack(config) {
+      if (process.env.ANALYZE) {
+        config.plugins.push(new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: 8888,
+          openAnalyzer: true
+        }))
+      }
+      return config
+    },
+    cssModules: true,
+    exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages')),
+  })
+)
