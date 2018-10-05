d2b7459c2a503eb36242aade2a3b5c537f4204a6
bglee <deptno@gmail.com>
2018-10-05 17:10:52 +0900

    [WIP] run next@7, remove storybook(dependency error), remove unused packages, fixes #16

--- .babelrc.js
+++
@@ -1,9 +0,0 @@
-const env      = {
-  'process.env.NODE_ENV': process.env.NODE_ENV
-}
-module.exports = {
-  presets: ['next/babel', '@zeit/next-typescript/babel'],
-  plugins: [['transform-define', env]]
-}
-
-
--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-url'),
-    require('postcss-cssnext'),
-    require('postcss-nested'),
-    require('postcss-browser-reporter'),
-    require('cssnano')({autoprefixer: false})
+    require('postcss-preset-env')()
   ]
 }
